d3936ae3dc7f394cd1e190615b9414774783c4b7
Minh Reigen <mreigen@gmail.com>
2018-07-02 12:15:11 -0700

    update README for base_url and change it to markdown

--- app/models/concerns/urlable.rb
+++ app/models/concerns/urlable.rb
@@ -1,22 +1,13 @@
 module Urlable
   extend ActiveSupport::Concern
 
-  def api_url
-    if Rails.env.production?
-      api_url = ENV['API_URL_PROD']
-    elsif Rails.env.development?
-      api_url = ENV['API_URL']
-    end
-    api_url
-  end
-
   def change_password_link user, text, token = nil
     "<a href='#{change_password_url(user, token)}'>#{text}</a>"
   end
 
   def change_password_url user, token = nil
     token ||= user.reset_password_token
-    "#{api_url}/users/#{user.name}/change_password?token=#{token}"
+    "#{ENV['BASE_URL']}/users/#{user.name}/change_password?token=#{token}"
   end
 
 end
